81f4a7a6ee1936ec35f7a852eefec5e24c675750
Matt11711 <matthew.d.allen97@gmail.com>
2022-11-19 12:52:00 -0500

    added nav tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -4,14 +4,14 @@ import "@testing-library/jest-dom/extend-expect";
 import About from "..";
 afterEach(cleanup);
 describe("About component", () => {
-    //renders About test
-    it('renders', () => {
-        render(<About />);
-    });
+  //renders About test
+  it("renders", () => {
+    render(<About />);
+  });
 
-    it("matches snapshot DOM node structure", () => {
-      // render About
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    });
-})
+  it("matches snapshot DOM node structure", () => {
+    // render About
+    const { asFragment } = render(<About />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
