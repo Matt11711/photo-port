2d038321bb6b00a578b4f7dfa818289141ff247a
Matt11711 <97207886+Matt11711@users.noreply.github.com>
2023-03-10 14:51:01 -0500

    about tests finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-
-import './App.css';
-import About from './components/About'
-import Nav from './components/Nav'
-
-function App() {
-  return (
-    <div >
-      <Nav></Nav>
-      <main>
-<About></About>
-</main>
-=======
 import React from "react";
 import "./App.css";
 import About from "./components/About/index";
@@ -23,7 +9,6 @@ function App() {
       <main>
         <About></About>
       </main>
->>>>>>> 7ac24e7b35fab62d8d76e52272c4cd5349bfa73e
     </div>
   );
 }
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import coverImage from "../../assets/cover/cover-image.jpg";
-const About = ()=> {
-return (
-    <section className="my-5">  
-        <h1 id="about">Who am I?</h1>
-        <img src={coverImage} className="my-2" style={{width: "100%"}} alt="cover"/>
-    </section>
-)}
-
-export default About
-=======
 import React from "react";
 import coverImage from "../../assets/cover/cover-image.jpg";
 const About = ()=> {
@@ -22,4 +9,3 @@ return (
   );
 }
 export default About;
->>>>>>> 7ac24e7b35fab62d8d76e52272c4cd5349bfa73e
